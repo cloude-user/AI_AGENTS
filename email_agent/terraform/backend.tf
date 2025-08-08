dcfe4091a495b63f674aa8b61bb6af714be9c39a
cloude-user <sundeep.cse.rymec@gmail.com>
2025-08-08 15:29:09 +0530

    feat: updated backend config

--- email_agent/terraform/backend.tf
+++ email_agent/terraform/backend.tf
@@ -5,7 +5,5 @@ terraform {
     bucket = "sundeep43-cloud-terraform43"
     key = "terraform/ai_agents/email_agent.tfstate"
     region = "us-east-2"
-    encrypt = true
-    use_lockfile = true
   }
 }
