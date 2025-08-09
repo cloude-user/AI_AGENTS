d7bbdd5c20f96171e256c35bd9e2f7086f238ceb
cloude-user <sundeep.cse.rymec@gmail.com>
2025-08-09 16:10:30 +0530

    terraform variables

--- email_agent/terraform/lambda/variables.tf
+++ email_agent/terraform/lambda/variables.tf
@@ -49,5 +49,6 @@ variable "ecr_uri" {
 }
 
 variable "gmail_json_token" {
-  type = string
+  type    = string
+  default = "{\"token\":\"ya29.A0AS3H6NyF0WPKbOYR_UbROZjHvKtbfyzXwxGZOZdK7O3unx6Rrnd0KJSRvf508Y-m8WgHPnbkEyvSJGlkZWiR1sGBBvHDfNNbfNgUGOQMFEqILo1ynP27uBMC630_6PRwfrFzSrZ36RJAiMZKGJtCd624h0ae0OhmEtg9ua9kMZSjKafOscnUh0RrJ1bA4Nlhf9B34ZoaCgYKAZsSARYSFQHGX2MiDZQaRHbDU0doryFdSr0bvA0206\",\"refresh_token\":\"1//0ggbHHNjfWegnCgYIARAAGBASNwF-L9Irdz8ESESYFA9SkSn1YSBlPOguIji-9X_ZVK8Gf7UYV1JV8WXIKv2jtoRakjJrEeH7VfU\",\"token_uri\":\"https://oauth2.googleapis.com/token\",\"client_id\":\"670645107880-5v1ghj5o05msm5n51vagg7b1tk799jqf.apps.googleusercontent.com\",\"client_secret\":\"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\",\"scopes\":[\"https://www.googleapis.com/auth/gmail.modify\"],\"universe_domain\":\"googleapis.com\",\"account\":\"\",\"expiry\":\"2025-08-02T10:19:10Z\"}"
 }
