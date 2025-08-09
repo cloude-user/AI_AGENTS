996c672f5b4f857186119c0bb5033cab338f1fc4
cloude-user <sundeep.cse.rymec@gmail.com>
2025-08-09 09:12:04 +0530

    edited env varibles

--- email_agent/terraform/lambda/variables.tf
+++ email_agent/terraform/lambda/variables.tf
@@ -1,13 +1,12 @@
 variable "aws_region" {
   type    = string
-  default = "us-east-1"
+  default = "us-east-2"
 }
 
 variable "lambda_role_name" {
   type    = string
   default = "gmail-agent-lambda-role"
 }
-
 
 # Image tag variable (can be set via TF_VAR_image_tag or pipeline)
 variable "image_tag" {
@@ -21,18 +20,30 @@ variable "openai_api_key" {
   default   = ""
 }
 
-variable "gmail_token"{
-
-} 
-variable "gmail_refresh_token {
-
+# Fixed: Missing closing quote
+variable "gmail_token" {
+  type      = string
+  sensitive = true
 }
-variable "gmail_client_id {
 
+# Fixed: Missing closing quote
+variable "gmail_refresh_token" {
+  type      = string
+  sensitive = true
 }
-variable "gmail_client_secret {
 
+# Fixed: Missing closing quote
+variable "gmail_client_id" {
+  type      = string
+  sensitive = true
 }
+
+# Fixed: Missing closing quote
+variable "gmail_client_secret" {
+  type      = string
+  sensitive = true
+}
+
 variable "ecr_uri" {
-
+  type = string
 }
